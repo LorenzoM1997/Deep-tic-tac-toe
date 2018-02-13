20a779cb125b7cdf4aee0bea1dd953ab1351854c
Lorenzo Mambretti <lorenz.m97@gmail.com>
2018-02-12 20:54:49 -0800

    improved training and neural network structure
    * updated tensorflow graph;
    * removed for loop from train();
    * improved saving for experience_replay;

--- ttt.py
+++ ttt.py
@@ -2,10 +2,6 @@
 Self-learning Tic Tac Toe
 Made by Lorenzo Mambretti and Hariharan Sezhiyan
 
-Last Update: 1/16/2018 9:05 PM (Lorenzo)
-    * tensorboard functionality
-    * finalized structure 
-    * improved train function
 """
 
 import random
@@ -88,22 +84,27 @@ def step(state, action):
     return 0, state_
 
 def save(W1, W2, B1, B2):
-    np.save("weights", sess.run(W1))
+    np.savez("weights.npz", W1, W2, B1, B2)
     print("file weights.txt has beeen updated successfully")
 
 def load():
-    fin = np.load('weights.npy')
+    npzfile = np.load("weights.npz")
+    W1 = np.reshape(npzfile['arr_0'], (27, 18))
+    W2 = np.reshape(npzfile['arr_1'], (18,9))
+    b1 = np.reshape(npzfile['arr_2'], (18))
+    b2 = np.reshape(npzfile['arr_3'], (9))
     return w1, w2, b1, b2
 
 def extract_policy(state):
     policy = None
+    q_values = compute_Q_values(state)
     for action in range(9):
         if is_valid(action,state):
             if policy == None:
                 policy = action
-                best_q = compute_Q_value(state, policy)
+                best_q = q_values[action]
             else:
-                new_q = compute_Q_value(state, action)
+                new_q = q_values[action]
                 if new_q > best_q:
                     policy = action
                     best_q = new_q
@@ -123,148 +124,152 @@ def invert_board(state):
     return state_
 
 def play_game():
-    while(true):
+    while(True):
         start_nb = input("If you would like to move first, enter 1. Otherwise, enter 2. ")
         start = int(start_nb)
         state = State()
         state.board = np.zeros((3,3))
-    
-        # human moves first
-        if(start == 1):
-            while(state.terminal != True):
-                move_nb = input("Please enter your move: ")
-                while(is_valid(int(move_nb), state) == False):
-                    move_nb = input("Please enter a correct move: ")
-                r, state = step(state, int(move_nb))
+
+        while not state.terminal:
+            if start == 1:
+                action = int(input("Please enter your move: "))
+                while(is_valid(action, state) == False):
+                    action = int(input("Please enter a correct move: "))
+                start = 0
+                r, state = step(state, action)
+            else:
+                state = invert_board(state)
+                action = extract_policy(state)
+                start = 1
+                r, state = step(state, action)
+                r = -r
+                state = invert_board(state)
+
+            print(state.board)
                 
-                if(state.terminal != True):
-                    action2 = extract_policy(state)
-                else:
-                    print("Game finished. You won.")
-                    break
-                state = invert_board(state)
-                r, state = step(state, action2)
-                state = invert_board(state)
-                if(state.terminal == True):
-                    print("Game finished. You lost.")
-                    break
-                print(state.board)
-    
-        # ai moves first
+        if r == 0:
+            print ("Tie")
+        elif r == 1:
+            print ("You won")
         else:
-            while(state.terminal != True):
-                action_pool = np.random.choice(9,9,replace = False)
-                action2 = extract_policy(state)
-                state = invert_board(state)
-                r, state = step(state, action2)
-                state = invert_board(state)
-    
-                if(state.terminal != True):
-                    move_nb = input("Please enter your move: ")
-                    while(is_valid(int(move_nb), state) == False):
-                        move_nb = input("Please enter a correct move: ")
-                else:
-                    print("Game finished. You lost.")
-                    break
-                r, state = step(state, int(move_nb))
-                if(state.terminal == True):
-                    print("Game finished. You won.")
-                    break
-                print(state.board)
+            print ("You lost")
 
+def convert_state_representation(state):
+    new_board = np.zeros(27)
+    for row in range(3):
+        for col in range(3):
+            if(state[row][col] == 0):
+                new_board[9 * row + 3 * col] = 1
+            elif(state[row][col] == 1):
+                new_board[9 * row + 3 * col + 1] = 1
+            else:
+                new_board[9 * row + 3 * col + 2] = 1
 
-def compute_Q_value(state,action):
+    return(new_board)
+
+def compute_Q_values(state):
     # computes associated Q value based on NN function approximator
-    q_board = np.copy(state.board)
-    q_board = np.reshape(q_board, (1,9))
-    action_board = np.zeros((1,9))
-    action_board[0][action] = 1
-    q_board = np.concatenate((q_board, action_board), axis=1)
+    q_board = np.zeros((1,27))
+    q_board = [np.copy(convert_state_representation(state.board))]
 
     #NN forward propogation
-    q_value = sess.run(y, feed_dict = {x: q_board})
-    return (q_value)
+    q_values = sess.run(y, feed_dict = {x: q_board})
+    q_values = np.reshape(q_values, 9)
+    return (q_values)
 
-def train(experience_replay):
-    
-    action_boards_ = np.zeros((576,9))
-    for i in range(576):
-        action_boards_[i][i % 9] = 1
-    
-    for i in range(100):
+def train(experience_replay, saved_W1, saved_W2, saved_b1, saved_b2):
+    # can modify batch size here
+    batch_size = 32
 
-        mini_batch = experience_replay[np.random.choice(experience_replay.shape[0], 64), :]
-        action_boards = np.zeros((64,9)) 
+    # take a random mini_batch
+    mini_batch = experience_replay[np.random.choice(experience_replay.shape[0], batch_size), :]
 
-        batch = np.reshape(np.array([o.board for o in mini_batch[:,0]]), (64, 9))
-        batch_ = np.reshape(np.array([o.board for o in mini_batch[:,3]]), (64, 9))
-        for i in range(64):
-            action_boards[i][mini_batch[i][1]] = 1
-        batch = np.concatenate((batch, action_boards), axis=1)
-        # batch will now be a 64 by 18 array
-        
-        batch_ = np.repeat(batch_, 9, axis=0)
-        batch_ = np.concatenate((batch_,action_boards_), axis=1)
-        # batch_ will now be a 576 by 18 array
-        
-        r = mini_batch[:,2] # is the list of all rewards within the mini_batch
-        q_t = sess.run(q_target, feed_dict = {x_old: batch_})
-        
-        summary, _ = sess.run([merged, train_step], feed_dict={x: batch, fixed_qt: q_t, reward : r})
-        train_writer.add_summary(summary, i)
+    # select state, state_, action, and reward from the mini batch
+    state = np.concatenate(mini_batch[:,0]).reshape((batch_size, -1))
+    act = np.array(mini_batch[:,1])
+    act = np.append([np.arange(batch_size)],[act], axis = 0)
+    act = np.transpose(act)
+    r = mini_batch[:,2]
+    state_ = np.concatenate(mini_batch[:,3]).reshape((batch_size, -1))
+    done = mini_batch[:,4]
     
-    return
+    # is the list of all rewards within the mini_batch
+        
+    summary, _= sess.run([merged, train_step], feed_dict={  x: state,
+                                                            x_old : state_,
+                                                            W1_old : saved_W1,
+                                                            W2_old : saved_W2,
+                                                            b1_old : saved_b1,
+                                                            b2_old : saved_b2,
+                                                            l_done : done,
+                                                            reward : r, 
+                                                            action_t : act
+                                                            })
+    train_writer.add_summary(summary)
 
 # Q learner neural network
 with tf.name_scope('Q-learner') as scope:
-    x = tf.placeholder(tf.float32, [None, 18], name='x')
+    x = tf.placeholder(tf.float32, [None, 27], name='x')
     with tf.name_scope('hidden_layer') as scope:
-        W1 = tf.Variable(tf.random_normal([18, 9]), name='W1')
-        b1 = tf.Variable(tf.random_normal([9]), name='b1')
+        W1 = tf.get_variable("W1", shape=[27, 18],
+           initializer=tf.contrib.layers.xavier_initializer())
+        b1 = tf.get_variable("b1", shape=[18],
+           initializer=tf.contrib.layers.xavier_initializer())
         h1 = tf.tanh(tf.matmul(x, W1) + b1)
     with tf.name_scope('output_layer') as scope:
-        W2 = tf.Variable(tf.random_normal([9, 1]), name='W2')
-        b2 = tf.Variable(tf.random_normal([1]), name='b2')
+        W2 = tf.get_variable("W2", shape=[18, 9],
+           initializer=tf.contrib.layers.xavier_initializer())
+        b2 = tf.get_variable("b2", shape=[9],
+           initializer=tf.contrib.layers.xavier_initializer())
         y = tf.tanh(tf.matmul(h1, W2) + b2)
+        action_t = tf.placeholder(tf.int32, [None, 2])
+
+    q_learner = tf.gather_nd(y, action_t)
 
 # Q target neural network
 with tf.name_scope('Q-target') as scope:
-    x_old = tf.placeholder(tf.float32, [None, 18], name='x_old')
+    x_old = tf.placeholder(tf.float32, [None, 27], name='x_old')
     with tf.name_scope('hidden_layer') as scope:
-        W1_old = tf.Variable(tf.random_normal([18, 9]), name='W1_old')
-        b1_old = tf.Variable(tf.random_normal([9]), name='b1_old')
+        W1_old = tf.placeholder(tf.float32, [27, 18], name='W1_old')
+        b1_old = tf.placeholder(tf.float32, [18], name='b1_old')
         h1_old = tf.tanh(tf.matmul(x_old, W1_old) + b1_old, name='h1')
     with tf.name_scope('output_layer') as scope:
-        W2_old = tf.Variable(tf.random_normal([9, 1]), name='W2_old')
-        b2_old = tf.Variable(tf.random_normal([1]), name='b2_old')
+        W2_old =tf.placeholder(tf.float32, [18, 9], name='W2_old')
+        b2_old =tf.placeholder(tf.float32, [9], name='b2_old')
         y_old = tf.tanh(tf.matmul(h1_old, W2_old) + b2_old, name='y_old')
-    q_target = tf.reduce_max(tf.reshape(y_old,[64,9]), axis = 1, keep_dims = True,)
+
+    l_done = tf.placeholder(tf.bool, [None])
+    reward = tf.placeholder(tf.float32, [None])
+    gamma = tf.constant(0.99, name='gamma')
+    qt = tf.reduce_max(y_old, axis = 1, name='maximum_qt')
+    q_target = tf.where(l_done, reward, reward + (gamma * qt), name='selected_max_qt')
 
 with tf.name_scope('loss') as scope:
-    fixed_qt = tf.placeholder(tf.float32, [None, 1])
-    reward = tf.placeholder(tf.float32, [None])
-    gamma = tf.constant(0.9, name='gamma')
-    squared_deltas = tf.square(reward + (gamma * fixed_qt) - y)
-    loss = tf.reduce_mean(squared_deltas)
+    loss = tf.losses.mean_squared_error(q_target, q_learner)
     
-train_step = tf.train.GradientDescentOptimizer(0.05).minimize(loss)
+#train_step = tf.train.GradientDescentOptimizer(0.03).minimize(loss)
+train_step = tf.train.RMSPropOptimizer(0.00025, momentum=0.95, use_locking=False, centered=False, name='RMSProp').minimize(loss)
 
 tf.summary.scalar('loss', loss)
 merged = tf.summary.merge_all()
 
 sess = tf.InteractiveSession()
-train_writer = tf.summary.FileWriter('C:/Users/Lorenzo Mambretti/AppData/Local/Programs/Python/Python35/Scripts/Output', sess.graph)
+train_writer = tf.summary.FileWriter('tensorflow_logs', sess.graph)
 tf.global_variables_initializer().run()
 
-episodes = 1000
+episodes = 100000
 n0 = 100.0
-experience_replay = np.zeros((0,4))
-count_experience = 0
+start_size = 500
+experience_replay = np.zeros((0,5))
+
+print("All set. Start epoch")
 
 for e in range(episodes):
     # print("episode ",e)
     state = State()
-    epsilon = n0 / (n0 + e)
+    if e >= start_size:
+        epsilon = max(n0 / (n0 + (e- start_size)), 0.1)
+    else: epsilon = 1
     
     if e % 2 == 1:
         # this is player 2's turn
@@ -284,7 +289,7 @@ for e in range(episodes):
             state = invert_board(state)
             r = -r 
     
-    while (state.terminal == False):
+    while not state.terminal:
         # this section is player 1's turn
         # select epsilon-greedy action
         if random.random() < epsilon:
@@ -300,7 +305,7 @@ for e in range(episodes):
 
         r, state_ = step(state, action)
 
-        if (state_.terminal == False):
+        if not state_.terminal:
             # this is player 2's turn
             state_ = invert_board(state_)
             if random.random() < epsilon:
@@ -318,25 +323,23 @@ for e in range(episodes):
             state_ = invert_board(state_)
             r = -r 
 
-        D0 = State()
-        D0.board = np.copy(state.board)
-        D0.terminal = state.terminal
-        D1 = State()
-        D1.board = np.copy(state_.board)
-        D1.terminal = state_.terminal
-        D = (D0, action, r, D1)
+        s = convert_state_representation(np.copy(state.board))
+        s_ = convert_state_representation(np.copy(state_.board))
+        done = state_.terminal
+        D = (s, action, r, s_, done)
         experience_replay = np.append(experience_replay, [D], axis = 0)
         state.board = np.copy(state_.board)
         state.terminal = state_.terminal
 
-    if(len(experience_replay) >= 128 and (e % 10) == 0):
-        print("Training")
-        train(experience_replay)
+    if e == start_size: print("Start Training")
+    if e >= start_size:
         if((e % 50) == 0):
-            print("Updating old network")
-            tf.assign(W1_old, W1)
-            tf.assign(W2_old, W2)
-            tf.assign(b1_old, b1)
-            tf.assign(b2_old, b2)
-
+            print("Episode:",e)
+            # here save the W1,W2,b1,B2
+            saved_W1 = W1.eval()
+            saved_W2 = W2.eval()
+            saved_b1 = b1.eval()
+            saved_b2 = b2.eval()
+        train(experience_replay, saved_W1, saved_W2, saved_b1, saved_b2)
+print("Training completed")
 play_game()
